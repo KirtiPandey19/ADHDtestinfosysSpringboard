00cf0258f2d5fc5dfa3f1c514b4a601d9aea0a16
Kirti Pandey <114803597+KirtiPandey19@users.noreply.github.com>
2024-12-01 20:11:00 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ADHDTest from "./ADHDTest"; // Updated import to ADHDTest
+
+function App() {
+  return (
+    <div>
+      <ADHDTest /> {/* Updated component name */}
+    </div>
+  );
+}
+
+export default App;
